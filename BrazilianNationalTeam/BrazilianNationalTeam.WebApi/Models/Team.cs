effdc813f9d317dbd8b3953e0de9b4831e55dc8d
goliveiragabriel <goliveiragabriel@gmail.com>
2020-02-26 08:54:18 -0300

    Meu Primeiro Commit
    Meu Primeiro Commit

---
+++ BrazilianNationalTeam/BrazilianNationalTeam.WebApi/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrazilianNationalTeam.WebApi.Models
+{
+    public class Team
+    {
+        //TODO: Colocar o ano da seleção
+        public string Name { get; set; }
+
+        public List<Player> Players { get; set; }
+
+        public Team()
+        {
+            Players = new List<Player>();
+        }
+    }
+}

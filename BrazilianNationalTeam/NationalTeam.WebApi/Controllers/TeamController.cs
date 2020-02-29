301ffb4efbfd76bc90a48d0ba8267322d554efd9
Daniel Roatt <danielroatt@gmail.com>
2020-02-29 11:25:39 -0300

    Selenium Completo
    Metodo do Selenium desenvolvido

--- BrazilianNationalTeam/NationalTeam.WebApi/Controllers/TeamController.cs
+++ BrazilianNationalTeam/NationalTeam.WebApi/Controllers/TeamController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NationalTeam.Models;
 using NationalTeam.Repositories;
 using NationalTeam.Repositories.Interfaces;
+using NationalTeam.WebApi.Helper;
+using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -32,7 +35,14 @@ namespace NationalTeam.WebApi.Controllers
             return _teamRepository.GetAll();
         }
 
-        [Route("api/[controller]/teste")]
+        [Route("api/[controller]/meuqueridotestejson")]
+        [HttpGet]
+        public string MeuQueridoTesteJson()
+        {
+            return "Vai Dani!!!!";
+        }
+
+        [Route("api/[controller]/param")]
         [HttpGet]
         public string Teste()
         {
@@ -44,9 +54,9 @@ namespace NationalTeam.WebApi.Controllers
         public string TesteSelenium()
         {
             var chromeOpt = new ChromeOptions();
-            
+
             chromeOpt.AddArguments("--incognito");
-            
+
             chromeOpt.AddArguments("--start-maximized");
 
             var driver = new ChromeDriver(chromeOpt);
@@ -54,7 +64,7 @@ namespace NationalTeam.WebApi.Controllers
             try
             {
                 driver.Navigate().GoToUrl("https://www.google.com.br/");
-                
+
                 driver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).SendKeys("MIDAS SOLUTIONS");
 
                 Thread.Sleep(TimeSpan.FromSeconds(2));
@@ -64,7 +74,7 @@ namespace NationalTeam.WebApi.Controllers
                 Thread.Sleep(TimeSpan.FromSeconds(2));
 
                 driver.FindElement(By.XPath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div[1]/a/h3")).Click();
-                
+
                 Thread.Sleep(TimeSpan.FromSeconds(5));
             }
             catch (Exception ex)
@@ -79,7 +89,68 @@ namespace NationalTeam.WebApi.Controllers
             return "Navegação Ok";
         }
 
+        [Route("api/[controller]/pegatimeano")]
+        [HttpPost]
+        public string PegaTimeAno([FromBody]RequisicaoTimeAno param)
+        {
+            var chromeOpt = new ChromeOptions();
+            chromeOpt.AddArguments("--incognito");
+            chromeOpt.AddArguments("--start-maximized");
+            //chromeOpt.AddArguments("--headless");
+
+            var driver = new ChromeDriver(chromeOpt);
+            
+            const string url = "https://rsssfbrasil.com/sel/national.htm";
+
+            try
+            {
+                var doc = new HtmlDocument();
+
+                string link;
+                string text;
+
+                for (var i = 1; i < 9; i++)
+                {
+                    for (var j = 1; j < 6; j++)
+                    {
+                        try
+                        {
+                            driver.Navigate().GoToUrl(url);
+
+                            link = driver.FindElement(By.XPath($"/html/body/table[2]/tbody/tr[{i}]/td[{j}]/a")).GetAttribute("href");
+                            text = driver.FindElement(By.XPath($"/html/body/table[2]/tbody/tr[{i}]/td[{j}]")).Text;
+
+                            driver.Navigate().GoToUrl(link);
+
+                            var team = driver.FindElement(By.XPath($"/html/body/table[1]/tbody/tr[3]/td/table/tbody/tr/td[1]/p[1]")).Text;
+
+                            var teamList = team.Split('\n');
+
+                            Console.WriteLine($"Link: {link} Texto: {text}");
 
 
+                            foreach (var player in teamList)
+                            {
+                                Console.WriteLine($"Jogador: {player}");
+                            }
+                        }
+                        catch 
+                        {
+                            Console.WriteLine("Sem Informação");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro ao navegar no Selenium. Mensagem: {e.Message} Stack: {e.StackTrace} ");
+            }
+            finally
+            {
+                driver?.Quit();
+            }
+
+            return JsonConvert.SerializeObject(param);
+        }
     }
 }
---
+++ BrazilianNationalTeam/NationalTeam.WebApi/Helper/RequisicaoTimeAno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NationalTeam.WebApi.Helper
+{
+    public class RequisicaoTimeAno
+    {
+        public string Ano { get; set; }
+    }
+}
